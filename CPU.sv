879edae5cb1b152a61b1f6fb2f7c082921842861
Parsa Hoseininejad <hoseininejad1999@gmail.com>
2019-05-11 20:23:30 +0430

    wrong name ir write

--- CPU.sv
+++ CPU.sv
@@ -5,5 +5,5 @@ module CPU (input clk, rst);
   wire [1:0] ALUop;
   wire IorD, srcA, srcB, lda, ldb, PCsrc, PCwrite, memRead, IRwrite, tos, pop, push, MtoS, PCwriteCond, memWrite;
   controller ctrl (clk, rst, opc, IorD, srcA, srcB, lda, ldb, PCsrc, PCwrite, memRead, IRwrite, tos, pop, push, MtoS, PCwriteCond, memWrite, ALUop);
-  dataPath DP (ALUop, PCwrite, PCwriteCond, IorD, memRead, memWrite, IRWrite, MtoS, push, pop, tos, lda, ldb, srcA, srcB, PCsrc, clk, rst, opc);
+  dataPath DP (ALUop, PCwrite, PCwriteCond, IorD, memRead, memWrite, IRwrite, MtoS, push, pop, tos, lda, ldb, srcA, srcB, PCsrc, clk, rst, opc);
 endmodule
