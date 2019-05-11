2de0b57387316618072fe8ea748f6b0ad411146b
Parsa Hoseininejad <hoseininejad1999@gmail.com>
2019-05-11 18:11:01 +0430

    assign

--- ALU.sv
+++ ALU.sv
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 
 module ALU(input[7:0] A, B, input[1:0] ALUOP, output[7:0] out);
-    out = ALUOP == 2'b00 ? A + B :
+    assign out = ALUOP == 2'b00 ? A + B :
           ALUOP == 2'b01 ? A - B :
           ALUOP == 2'b10 ? A & B : 
           ALUOP == 2'b11 ? ~A : 
