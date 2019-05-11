dd2f6c97be7d8281fe01570a2ffac9c324d167ed
nmgtav <nima.mg@gmail.com>
2019-05-11 20:11:30 +0430

    last commit for tonight :)) try to debug dude

--- stack.sv
+++ stack.sv
@@ -1,16 +1,16 @@
 `timescale 1ns/1ns
 
 module stack(input[7:0] din, input push, pop, tos, clk, rst, output reg[7:0] dout);
-    reg[7:0] mem [31:0];
+    reg[7:0] mem [0:31];
     reg[4:0] top;
     always @(posedge clk, posedge rst) begin
         if (rst) begin
-            top <= 0;
             integer i;
-            for (i = 0 ; i < 32 ; i++) begin
+            for (i = 0 ; i < 32 ; i = i + 1) begin
               mem[i] = 0;
             end
-          end
+            top <= 0;
+        end
         else begin
             if (push) begin
                 top <= top + 1;
