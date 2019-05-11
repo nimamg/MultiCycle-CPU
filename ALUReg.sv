1670861e3356588ca467f5a89d0581d28351d038
Parsa Hoseininejad <hoseininejad1999@gmail.com>
2019-05-11 17:20:31 +0430

    alu reg

---
+++ ALUReg.sv
@@ -0,0 +1,10 @@
+`timescale 1ns/1ns
+
+module ALUREG(input[7:0] inp, clk, rst, output[7:0] out);
+    always @(posedge rst, posedge clk) begin
+        if (clk)
+            out <= 8'b0;
+        else 
+            out <= inp;
+    end
+endmodule
