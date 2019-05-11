a238d6e686c6a757e9cb1d2cf5aec3061814d4a0
Parsa Hoseininejad <hoseininejad1999@gmail.com>
2019-05-11 17:16:42 +0430

    alu

---
+++ ALU.sv
@@ -0,0 +1,9 @@
+`timescale 1ns/1ns
+
+module ALU(input[7:0] A, B, input[1:0] ALUOP, output[7:0] out);
+    out = ALUOP == 2'b00 ? A + B :
+          ALUOP == 2'b01 ? A - B :
+          ALUOP == 2'b10 ? A & B : 
+          ALUOP == 2'b11 ? ~A : 
+          out;
+endmodule
